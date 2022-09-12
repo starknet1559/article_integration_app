b744b6a4b79f9dcbd99db106cb3bbc12466cb92f
EC2 Default User <ec2-user@ip-172-31-30-140.ec2.internal>
2022-09-12 15:09:45 +0000

    Ajax

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,16 +2,14 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.post_id = post.id
+    @comment.save
   end
 
   def destroy
-    comment = PostComment.find(params[:id])
-    comment.destroy
-    redirect_to post_path(params[:post_id])
+    @comment = PostComment.find_by(id: params[:id], post_id: params[:post_id])
+    @comment.destroy
   end
 
    private
--- app/controllers/public/post_goods_controller.rb
+++ app/controllers/public/post_goods_controller.rb
@@ -2,19 +2,17 @@ class Public::PostGoodsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    post_good = current_user.post_goods.new(post_id: post.id)
+    @post_good = current_user.post_goods.new(post_id: post.id)
     if post_bad = current_user.post_bads.find_by(post_id: post.id)
       post_bad.destroy
     end
-    post_good.save
-    redirect_to request.referer
+    @post_good.save
   end
 
   def destroy
     post = Post.find(params[:post_id])
-    post_good = current_user.post_goods.find_by(post_id: post.id)
-    post_good.destroy
-    redirect_to request.referer
+    @post_good = current_user.post_goods.find_by(post_id: post.id)
+    @post_good.destroy
   end
 
 end
